f34f57bf7de03bf268128220138efe8001780c29
jer <alphastrata@gmail.com>
2024-08-19 10:43:38 +1000

    meh

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,29 @@
 use num_format::{Locale, ToFormattedString};
-use prettytable::{color, format, Attr, Cell, Row, Table};
+use prettytable::{format, Cell, Row, Table};
 
 fn main() {
     let mut table = Table::new();
 
+    // Set table format to match Markdown table format with proper spacing
     table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
 
-    let lower_powers: Vec<(u32, u64)> = (4..=31).map(|x| (x, 2u64.pow(x))).collect();
-    let upper_powers: Vec<(u32, u64)> = (32..=63).map(|x| (x, 2u64.pow(x))).collect();
+    // Header row for the Markdown table
+    table.add_row(Row::new(vec![Cell::new("| Power | Number | Bitshift |")]));
+    table.add_row(Row::new(vec![Cell::new("| --- | --- | --- |")]));
 
-    for (i, _) in lower_powers.iter().enumerate() {
-        let (n1, val1) = lower_powers[i];
-        let (n2, val2) = upper_powers[i];
+    let powers: Vec<(u32, u64)> = (4..=63).map(|x| (x, 2u64.pow(x))).collect();
 
-        let color_style = if i % 2 == 0 {
-            color::BRIGHT_CYAN
-        } else {
-            color::BRIGHT_YELLOW
-        };
-
-        let row = Row::new(vec![
-            Cell::new(&format!(
-                "2^{} | {:<20}",
-                n1,
-                val1.to_formatted_string(&Locale::en)
-            ))
-            .with_style(Attr::ForegroundColor(color_style)),
-            Cell::new(&format!(
-                "2^{} | {:<20}",
-                n2,
-                val2.to_formatted_string(&Locale::en)
-            ))
-            .with_style(Attr::ForegroundColor(color_style)),
-        ]);
+    for (n, val) in powers.iter() {
+        let row = Row::new(vec![Cell::new(&format!(
+            "| 2^{} | {} | 1u << {}u |",
+            n,
+            val.to_formatted_string(&Locale::en),
+            n
+        ))]);
 
         table.add_row(row);
     }
 
+    // Print the table in Markdown format
     table.printstd();
 }
